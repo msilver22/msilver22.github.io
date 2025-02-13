6905d3ab908a9362298521e4179bf853890fe8cf
msilver22 <151758027+msilver22@users.noreply.github.com>
2025-02-13 16:50:22 +0000

    Deploying to gh-pages from @ msilver22/msilver22.github.io@904f8f28903c1a5d6bf37909b5d1ee941524648c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,13 +445,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/matteo-silvestri-22-peedmat", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
