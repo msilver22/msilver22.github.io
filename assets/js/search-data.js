fbb0371901720abf5a10153e7fd2733f4e06ed28
msilver22 <151758027+msilver22@users.noreply.github.com>
2025-02-13 15:39:53 +0000

    Deploying to gh-pages from @ msilver22/msilver22.github.io@341fb528ebfa471fb47dc84cff8e29aaea0a39c5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -403,16 +403,11 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+    },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-starting-a-ph-d-in-computer-science-at-sapienza-university-of-rome",
+          title: 'Starting a Ph.D. in Computer Science at Sapienza University of Rome!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
