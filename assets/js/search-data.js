faf871d8df784381c64239d32fcae7a1917c59df
msilver22 <151758027+msilver22@users.noreply.github.com>
2025-03-20 10:45:55 +0000

    Deploying to gh-pages from @ msilver22/msilver22.github.io@4ac3f6f7e482d4aea7efac22714088584ba42dc1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -424,55 +424,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-discord',
-        title: 'Discord',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://discord.com/users/1081636787220398261", "_blank");
-        },
-      },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%6D.%73%69%6C%76%65%73%74%72%69@%75%6E%69%72%6F%6D%61%31.%69%74", "_blank");
-        },
-      },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/msilver22", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/matteo-silvestri-22-peedmat", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=ET07P-kAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-x',
-        title: 'X',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://twitter.com/Silver22_2k", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://gabrieletolomei.netlify.app/#hercole-lab", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
