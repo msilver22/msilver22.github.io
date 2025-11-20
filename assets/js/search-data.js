70a44e08d37e1e26ec7e045130923eac2a4bd8db
msilver22 <151758027+msilver22@users.noreply.github.com>
2025-11-20 10:12:45 +0000

    Deploying to gh-pages from @ msilver22/msilver22.github.io@dbac9ffb357f69ddabe9349b362d4b9d1cb803fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,17 +34,6 @@ ninja.data = [{
           window.location.href = "/blog/2024/photo-gallery/";
         
       },
-    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
     },{id: "post-a-post-with-tabs",
       
         title: "a post with tabs",
@@ -265,17 +254,6 @@ ninja.data = [{
           window.location.href = "/blog/2022/giscus-comments/";
         
       },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-        
-      },
     },{id: "post-a-post-with-redirect",
       
         title: "a post with redirect",
@@ -389,6 +367,9 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "news-check-out-my-first-publication-exploring-the-potential-of-artificial-intelligence-in-assessing-the-risk-of-gastric-neoplastic-lesions-in-patients-with-corpus-atrophic-gastritis-to-the-gastric-cancer-journal",
           title: 'Check out my first publication Exploring the potential of artificial intelligence in assessing...',
+          description: "",
+          section: "News",},{id: "news-happy-to-share-that-we-receive-the-best-paper-award-at-the-hcai-workshop",
+          title: 'Happy to share that we receive the Best Paper Award at the HCAI...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
